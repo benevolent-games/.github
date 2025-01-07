e25495d060f97e46550ca213aadd0f2d6e464336
Chase Moskal <chasemoskal@gmail.com>
2025-01-07 01:49:09 -0800

    fix: rogue crusade url, also tweak headings

--- s/games.ts
+++ s/games.ts
@@ -4,17 +4,17 @@ import {html} from "@benev/turtle"
 export const games = {
 
 	rogue: {
-		label: "ROGUE CRUSADE",
+		label: "Rogue Crusade",
 		motto: "The Ultimate Roguelike",
 		poster: "/assets/posters/rogue.avif",
-		url: "https://rogue.benev.gg/",
+		url: "https://rogue.benevolent.games/",
 		content: () => html`
 			<p>We're building a new 1990s roguelike for 2025. It'll be a co-op/solo dungeon crawler with permadeath and procgen.</p>
 		`,
 	},
 
 	regis: {
-		label: "REGIS.gg",
+		label: "Regis.gg",
 		motto: "A New Kind of 1v1 Strategy Game",
 		poster: "/assets/posters/regis.avif",
 		url: "https://regis.gg/",
